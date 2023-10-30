9098af729a7114b778ac867d51ed95cc8ccf2a14
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-10-30 14:58:22 +0000

    Rodape criado

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -7,6 +7,9 @@
     <Stack />
     <Biography />
   </main>
+  <footer>
+    <Baseboard />
+  </footer>
 </template>
 
 <script>
@@ -14,13 +17,15 @@ import Navbar from "../components/Global/Navbar.vue"
 import Banner from "../components/Home/Banner.vue"
 import Stack from "../components/Home/Stack.vue"
 import Biography from "../components/Home/Biography.vue"
+import Baseboard from "../components/Global/Baseboard.vue"
 export default {
   name: "Home",
   components: {
     Navbar,
     Banner,
     Stack,
-    Biography
+    Biography, 
+    Baseboard
   }
 }
 </script>
---
+++ src/components/Global/Baseboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="bg-black text-orange-500 text-center p-2.5">
+    <Line />
+    <h1 class="mt-2.5">&copy; {{ year }} All Rights Reserved</h1>
+    <Networks/>
+  </footer>
+</template>
+
+<script>
+  import Networks from "./Networks.vue"
+  import Line from "./Line.vue"
+export default {
+  name: "Baseboard",
+  data() {
+    return {
+      year: ""
+    }
+  },
+  created() {
+    this.getYear()
+  },
+  methods: {
+    getYear() {
+      const currentDate = new Date()
+      this.year = currentDate.getFullYear()
+    }
+  },
+  components: { Networks, Line }
+}
+</script>
