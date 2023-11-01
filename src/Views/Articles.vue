c708b0190c8889961d7b15b73f5a4d0faa77d7da
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-11-01 20:12:38 +0000

    Rodape da página de artigos

--- src/Views/Articles.vue
+++ src/Views/Articles.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="bg-blue-200">
+  <main class="bg-blue-200">
     <Navbar />
     <Search />
     <Category />
     <div class="overflow-x-auto flex pr-8 pl-2">
       <Card v-for="article in articles" :key="article.id" :article="article" />
     </div>
-  </div>
+    <Baseboard />
+  </main>
 </template>
 
 <script>
@@ -14,6 +15,7 @@ import Navbar from "../components/Articles/Navbar.vue"
 import Search from "../components/Articles/Search.vue"
 import Category from "../components/Articles/Category.vue"
 import Card from "../components/Articles/Card.vue"
+import Baseboard from "../components/Articles/Baseboard.vue"
   import articles from "../../articles.json"
 export default {
   name: "Articles",
@@ -21,7 +23,8 @@ export default {
     Navbar,
     Search, 
     Category,
-    Card
+    Card,
+    Baseboard
   },
   data() {
     return {
---
+++ src/components/Articles/Baseboard.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div class="flex justify-around p-5">
+      <div class="flex flex-col text-center py-2">
+        <i class="pi pi-home text-blue-500" style="font-size: 1.5rem;"></i>
+        <p class="text-blue-500 font-bold">HOME</p>
+      </div>
+      <div class="flex flex-col text-center py-2">
+      <i class="pi pi-heart text-blue-500" style="font-size: 1.5rem;"></i>
+        <p class="text-blue-500 font-bold">FAVORITE</p>
+      </div>
+      <div class="flex flex-col text-center py-2">
+      <i class="pi pi-comment text-blue-500" style="font-size: 1.5rem;"></i>
+      <p class="text-blue-500 font-bold">COMMENTS</p>
+      </div>
+      <div class="flex flex-col text-center py-2">
+      <i class="pi pi-cog text-blue-500" style="font-size: 1.5rem;"></i>
+        <p class="text-blue-500 font-bold">A
+        CONFIG</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "Baseboard"
+  }
+</script>
