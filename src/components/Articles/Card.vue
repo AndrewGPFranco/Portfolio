3442b465d7d8155724aa40d1a25d00cd2e3445ca
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-11-02 22:03:35 +0000

    Limitando os caracteres da descrição no card

--- src/components/Articles/Card.vue
+++ src/components/Articles/Card.vue
@@ -4,7 +4,7 @@
       <img class="h-52 rounded-t-2xl" :src="article.image" alt="Imagem" />
       <div class="p-6 bg-gray-200 text-black rounded-b-2xl">
         <h1 class="font-bold text-xl pb-1">Language {{ article.title }}</h1>
-        <p>{{ article.description.substring(0,250) }}</p>
+        <p>{{ article.description.substring(0,217) + "..."}}</p>
         <p class="font-bold text-end">{{ article.author }}</p>
       </div>
     </div>
