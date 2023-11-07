1d8c764f0862ee660b3582a8a6c15fb087eab58f
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-11-07 12:41:36 +0000

    Finalizado por enquanto a tela de artigos

--- src/components/Articles/Baseboard.vue
+++ src/components/Articles/Baseboard.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="px-1 pt-5 pb-3 flex justify-center">
-    <div class="flex justify-around p-3 border border-black rounded-full w-2/5">
+    <div class="flex justify-around p-1 border border-gray-400 rounded-full w-2/5">
       <div class="flex flex-col text-center py-2">
         <router-link to="/"><i class="pi pi-home text-black" style="font-size: 1.5rem;"></i>
         <p class="text-black font-bold">HOME</p></router-link>
--- src/components/Articles/Card.vue
+++ src/components/Articles/Card.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="py-1 px-2 min-w-full">
     <div class="rounded-2xl bg-black flex flex-col">
-      <img class="h-52 rounded-t-2xl" :src="article.image" alt="Imagem" />
-      <div class="p-6 bg-gray-200 text-black rounded-b-2xl">
+      <img class="h-48 rounded-t-2xl" :src="article.image" alt="Imagem" />
+      <div class="h-52 p-6 bg-gray-200 text-black rounded-b-2xl">
         <h1 class="font-bold text-xl pb-1">{{ article.title }}</h1>
-        <p>{{ article.description.substring(0,200) + "..."}}</p>
+        <p>{{ article.description.substring(0,170) + "..."}}</p>
         <p class="font-bold text-end">{{ article.author }}</p>
       </div>
     </div>
