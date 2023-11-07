2488f7a29e1a428795f23352305423551c4378ba
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-11-07 12:26:23 +0000

    Up

--- src/components/Articles/Baseboard.vue
+++ src/components/Articles/Baseboard.vue
@@ -1,13 +1,9 @@
 <template>
-  <section class="px-1 pt-5 pb-3">
-    <div class="flex justify-around p-5 border border-black rounded-full">
+  <section class="px-1 pt-5 pb-3 flex justify-center">
+    <div class="flex justify-around p-3 border border-black rounded-full w-2/5">
       <div class="flex flex-col text-center py-2">
         <router-link to="/"><i class="pi pi-home text-black" style="font-size: 1.5rem;"></i>
         <p class="text-black font-bold">HOME</p></router-link>
-      </div>
-      <div class="flex flex-col text-center py-2">
-      <router-link to="/"><i class="pi pi-heart text-black" style="font-size: 1.5rem;"></i></router-link>
-        <p class="text-black font-bold">FAVORITE</p>
       </div>
     </div>
   </section>
--- src/components/Articles/Card.vue
+++ src/components/Articles/Card.vue
@@ -3,8 +3,8 @@
     <div class="rounded-2xl bg-black flex flex-col">
       <img class="h-52 rounded-t-2xl" :src="article.image" alt="Imagem" />
       <div class="p-6 bg-gray-200 text-black rounded-b-2xl">
-        <h1 class="font-bold text-xl pb-1">Language {{ article.title }}</h1>
-        <p>{{ article.description.substring(0,217) + "..."}}</p>
+        <h1 class="font-bold text-xl pb-1">{{ article.title }}</h1>
+        <p>{{ article.description.substring(0,200) + "..."}}</p>
         <p class="font-bold text-end">{{ article.author }}</p>
       </div>
     </div>
