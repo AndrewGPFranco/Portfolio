8ebb12f3f1ae4fe675aefdd5381ed6bea3962ce2
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-11-11 00:48:19 +0000

    foto adicionada

--- src/components/Projects/Card.vue
+++ src/components/Projects/Card.vue
@@ -2,7 +2,7 @@
   <section class="flex justify-center flex-col">
     <div v-if="aulas" class="text-center p-5 border border-gray-100 m-2 rounded-full" v-for='aula in aulas' :key="aula.id">
       <img class="flex w-2/4 m-auto justify-center" :src="aula.image" alt="Imagem do Projeto" />
-      <h1 class="text-center mt-3">{{ aula.title }}</h1>
+      <h1 class="text-center text-white mt-3">{{ aula.title }}</h1>
       <p class="text-gray-300">{{ aula.description }}</p>
       <p class="text-gray-300">Category: <span class="text-orange-500">{{ aula.category }}</span></p>
     </div>
@@ -10,6 +10,7 @@
       <h1 class="text-sky-400">
         Essa página utiliza de uma Api própria porém não hospedada, mais pra frente planejo hospedar mas enquanto isso, pode ver o resultado em meu canal do tiktok @gps_drew_dev
       </h1>
+      <img class="mt-3" src="../../assets/projetos-api.png" alt="Imagem do Site" />
     </div>
   </section>
 </template>
@@ -24,7 +25,7 @@
       }
     },
     created() {
-      axios.get('http://127.0.0.1:8000/api/aulas')
+      axios.get('http://127.0.0.1:8000/api/aul')
         .then((response) => {
           console.log(response)
           this.aulas = response.data
