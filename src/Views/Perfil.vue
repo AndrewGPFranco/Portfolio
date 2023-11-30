7c8ff2560e6fe315cab93e6f1a3307c83d9357e0
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-11-30 14:08:50 +0000

    Protótipo do perfil finalizado

--- src/Views/Perfil.vue
+++ src/Views/Perfil.vue
@@ -2,18 +2,21 @@
   <Banner />
   <Photography/>
   <Information/>
+  <Baseboard />
 </template>
 
 <script>
   import Banner from "../components/Perfil/Banner.vue"
   import Photography from "../components/Perfil/Photography.vue"
   import Information from "../components/Perfil/Information.vue"
+  import Baseboard from "../components/Global/Baseboard.vue"
   export default {
     name: "Perfil",
     components: {
       Banner, 
       Photography,
-      Information
+      Information,
+      Baseboard
     }
   }
 </script>
--- src/components/Perfil/Photography.vue
+++ src/components/Perfil/Photography.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="bg-black flex justify-center">
-    <img class="-mt-10 w-36 h-36 rounded-full" src="../../assets/user.jpg" alt="User Photography" />
+    <img class="-mt-10 w-40 h-40 rounded-full" src="../../assets/user.jpg" alt="User Photography" />
   </section>
 </template>
 
