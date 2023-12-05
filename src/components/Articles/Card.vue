70829225a182e6cdfe690bf2da77511ea3f38d1a
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-12-05 12:18:40 +0000

    Melhorando a interatividade do usuário na tela de artigos

--- src/components/Articles/Card.vue
+++ src/components/Articles/Card.vue
@@ -2,9 +2,13 @@
   <section class="py-1 px-2 min-w-full">
     <div class="rounded-2xl bg-black flex flex-col">
       <img class="h-48 rounded-t-2xl" :src="article.image" alt="Imagem" />
-      <div class="h-52 p-6 bg-gray-200 text-black rounded-b-2xl">
+      <div id="card" class="h-auto p-6 bg-gray-200 text-black rounded-b-2xl">
         <h1 class="font-bold text-xl pb-1">{{ article.title }}</h1>
-        <p>{{ article.description.substring(0,170) + "..."}}</p>
+        <p v-if="!readmore">{{ article.description.substring(0,170) + "..."}}</p>
+        <p v-if="readmore">{{ article.description }}</p>
+        <button @click="activeRead" class="mt-2 text-center rounded-xl p-2 bg-blue-500 text-black font-bold">
+          Read More
+        </button>
         <p class="font-bold text-end">{{ article.author }}</p>
       </div>
     </div>
@@ -19,6 +23,23 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  data() {
+    return {
+      readmore: false
+    }
+  },
+  methods: {
+    activeRead() {
+      this.readmore = !this.readmore;
+    }
   }
 }
 </script>
+
+<style scoped>
+  #card {
+    overflow-y: auto;
+    max-height: 250px;
+  }
+</style>
