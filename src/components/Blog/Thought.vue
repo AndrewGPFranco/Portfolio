e6dcd55e997c22d538c8d56c86ff0d576f6aba65
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-12-07 13:54:45 +0000

    Up

--- src/components/Blog/Thought.vue
+++ src/components/Blog/Thought.vue
@@ -1,10 +1,12 @@
 <template>
+  <section>
     <div class="p-6" v-for="thought in thoughts" :key="thought.id">
-      <h1 class="font-bold text-2xl text-start underline">{{ thought.title.toUpperCase() }}</h1>
-      <img class="h-44 rounded-xl mt-2 w-full" v-if="thought.image" :src="thought.image" alt="Image of thought" />
+      <h1 class="font-bold text-xl text-start underline">{{ thought.title.toUpperCase() }}</h1>
+      <img class="h-48 rounded-xl mt-2 w-full" v-if="thought.image" :src="thought.image" alt="Image of thought" />
       <p class="mt-3">{{ thought.description }}</p>
       <h3 class="text-end text-blue-500"><i class="pi pi-calendar"></i> {{ thought.date }}</h3>
     </div>
+  </section>
 </template>
 
 <script>
