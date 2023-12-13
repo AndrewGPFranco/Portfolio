07094f3ea82032d46b3289bdc14aaf472c3b2773
AndrewGPSilva <125920218+AndrewGPSilva@users.noreply.github.com>
2023-12-13 20:11:15 +0000

    Up

--- src/Views/NotFound.vue
+++ src/Views/NotFound.vue
@@ -1,8 +1,11 @@
 <template>
-  <section class="bg-black p-10 text-center align-items">
+  <!--<section class="bg-black p-10 text-center align-items">
     <h1 class="text-center font-bold text-4xl text-red-500">
     404 PAGE NOT FOUND
     </h1>
+  </section>-->
+  <section>
+    
   </section>
 </template>
 
@@ -17,5 +20,9 @@
 <style scoped>
   section {
     height: 100vh;
+    width: 100vw;
+    background-image: url("../assets/pnf.PNG");
+    background-size: cover;
+    background-position: center;
   }
 </style>
