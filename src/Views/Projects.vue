7cb05fb97d07b5bdd856a53ae877375cb25c1304
Andrew Gomes <andrewgomes1328@gmail.com>
2023-12-26 00:01:30 -0300

    componente para projetos backend, utilizando um iframe para mostrar o video do projeto

--- src/Views/Projects.vue
+++ src/Views/Projects.vue
@@ -4,6 +4,7 @@
   </header>
   <main class="py-4 px-3 bg-black">
     <h1 class="-mt-3 mb-2 text-blue-300 text-center font-bold text-4xl">my PROJECTS</h1>
+    <CardVideo />
     <Card />
   </main>
   <footer>
@@ -14,6 +15,7 @@
 <script>
   import Baseboard from "../components/Global/Baseboard.vue"
   import Card from "../components/Projects/Card.vue"
+  import CardVideo from "../components/Projects/CardVideo.vue"
   import BackHome from "../components/Global/BackHome.vue"
   import Navbar from "../components/Projects/Navbar.vue"
   export default {
@@ -22,7 +24,8 @@
       Baseboard,
       Card,
       Navbar,
-      BackHome
+      BackHome,
+      CardVideo
     }
   }
 </script>
