1a446ae8bd6c3295bd57e38fadff40b34f48d969
Andrew Gomes <125920218+AndrewGPFranco@users.noreply.github.com>
2024-09-22 22:44:25 +0000

    Ajustando minha idade na biografia

--- src/components/Home/Biography.vue
+++ src/components/Home/Biography.vue
@@ -2,7 +2,7 @@
   <section class="bg-blue-900 p-7 mt-2">
     <div id="container" class="p-2 mt-6">
       <p class="text-gray-300 text-start mb-5 text-xl">
-Hello!  I'm Andrew, I'm 22 years old and I'm a versatile developer with an appreciation for the frontend and a heart for the backend!  My Front-End skills cover HTML, CSS, JavaScript, Vue, Typescript, Bootstrap and Tailwind while in the Back-End, my skills are in Java, Spring. I have comprehensive knowledge in Docker, MySql and PostgreSQL databases and I am highly skilled in versioning with Git and GitHub.
+Hello!  I'm Andrew, I'm 23 years old and I'm a versatile developer with an appreciation for the frontend and a heart for the backend!  My Front-End skills cover HTML, CSS, JavaScript, Vue, Typescript, Bootstrap and Tailwind while in the Back-End, my skills are in Java, Spring. I have comprehensive knowledge in Docker, MySql and PostgreSQL databases and I am highly skilled in versioning with Git and GitHub.
       </p>
     </div>
     <div class="h-56"></div>
